b79d19f0f2f23cd44e93dc32abbcba9e95e11374
ewerton Mattos <ewerton.sc@gmail.com>
2022-11-02 20:31:07 -0300

    enablin http and https for the Service

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -21,14 +21,14 @@ namespace GrpcClient
 
         public static void CallGrpcService()
         {
-            //string serviceUrl = System.Environment.GetEnvironmentVariable("GrpcServiceUrl");
+            string serviceUrl = System.Environment.GetEnvironmentVariable("GrpcServiceUrl");
 
-            //AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 
-            //var channel = GrpcChannel.ForAddress(serviceUrl);
-            //var client = new Greeter.GreeterClient(channel);
-            //var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
-            //Console.WriteLine("Greeting: " + reply.Message);
+            var channel = GrpcChannel.ForAddress(serviceUrl);
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
